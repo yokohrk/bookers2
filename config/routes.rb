b137c3ca232642369eae15be2d4929fa48033356
Hiroyuki Yokoi <2282382+yokohrk@users.noreply.github.com>
2021-10-30 03:05:42 +0000

    必要なファイルの準備まで完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'homes/about'
   devise_for :users
   root to: 'homes#top'
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update, :index]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_22_065836) do
+ActiveRecord::Schema.define(version: 2021_10_29_060924) do
+
+  create_table "books", force: :cascade do |t|
+    t.text "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -19,6 +27,8 @@ ActiveRecord::Schema.define(version: 2021_10_22_065836) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.text "introduction"
+    t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
