0a2a50437236188000840176eb1f0376773433ed
Hiroyuki Yokoi <2282382+yokohrk@users.noreply.github.com>
2021-11-07 10:32:09 +0000

    deploy setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'homes/about'
+  get 'home/about' => 'homes#about'
   devise_for :users
   root to: 'homes#top'
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+  resources :books, only: [:create, :index, :show, :destroy, :edit, :update]
   resources :users, only: [:show, :edit, :update, :index, :info]
 end
